dfe434a8f810f55c2b8add52ef859bb5351e51a3
marjune <marjune@163.com>
2019-12-27 15:45:07 +0800

    refactor: extract request handler

---
+++ src/handler.js
@@ -0,0 +1,90 @@
+const url = require('url');
+
+const corsHeaders = {
+	'access-control-allow-credentials': 'true',
+	'access-control-allow-origin': '*',
+	'access-control-allow-methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS, TRACE, CONNECT'
+};
+
+function getInitData(query) {
+	const status = isFinite(query.status) ? parseInt(query.status) : 200;
+
+	const headers = {};
+
+	if (query.cors !== undefined) {
+		Object.assign(headers, corsHeaders);
+		const reqCorsHeaders = req.headers['access-control-request-headers'];
+		if (reqCorsHeaders) {
+			headers['access-control-allow-headers'] = reqCorsHeaders;
+		}
+	}
+
+	if (query.type) {
+		headers['content-type'] = query.type;
+	}
+
+	if (query.location) {
+		headers['location'] = query.location;
+	}
+
+	headers['date'] = (new Date()).toUTCString();
+
+	const body = query.body || '';
+
+	return {
+		status,
+		headers,
+		body
+	}
+}
+
+function onRequest(req, res) {
+	/*
+	query
+	 - stuck
+	 - reset
+	 - silent
+	 - wait
+
+	 - status
+	 - cors
+	 - location
+	 - type
+	 - body
+	 */
+
+	const socket = req.socket;
+	const reqUrl = url.parse(req.url, true);
+	const query = reqUrl.query;
+
+	if (query.stuck !== undefined) {
+		return;
+	}
+
+	const serve = function() {
+		if (query.reset !== undefined) {
+			socket.destroy();
+			return;
+		}
+
+		if (query.silent !== undefined) {
+			socket.end();
+			return;
+		}
+
+		const {status, headers, body} = getInitData(req, query);
+
+		res.writeHead(status, headers);
+		res.end(body);
+	};
+
+	if (isFinite(query.wait)) {
+		const wait = parseInt(query.wait);
+		setTimeout(serve, wait);
+		return;
+	}
+
+	serve();
+}
+
+module.exports = onRequest;
--- src/index.js
+++ src/index.js
@@ -1,99 +1,12 @@
 #!/usr/bin/env node
 const http = require('http');
 const https = require('https');
-const url = require('url');
 const fs = require('fs');
 
 const args = require('./args');
-const appOptions = args.parse();
-const {host, port, cert, key} = appOptions;
+const requestHandler = require('./handler');
 
-const corsHeaders = {
-	'access-control-allow-credentials': 'true',
-	'access-control-allow-origin': '*',
-	'access-control-allow-methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS, TRACE, CONNECT'
-};
-
-function getInitData(query) {
-	const status = isFinite(query.status) ? parseInt(query.status) : 200;
-
-	const headers = {};
-
-	if (query.cors !== undefined) {
-		Object.assign(headers, corsHeaders);
-		const reqCorsHeaders = req.headers['access-control-request-headers'];
-		if (reqCorsHeaders) {
-			headers['access-control-allow-headers'] = reqCorsHeaders;
-		}
-	}
-
-	if (query.type) {
-		headers['content-type'] = query.type;
-	}
-
-	if (query.location) {
-		headers['location'] = query.location;
-	}
-
-	headers['date'] = (new Date()).toUTCString();
-
-	const body = query.body || '';
-
-	return {
-		status,
-		headers,
-		body
-	}
-}
-
-function onRequest(req, res) {
-	/*
-	query
-	 - stuck
-	 - reset
-	 - silent
-	 - wait
-
-	 - status
-	 - cors
-	 - location
-	 - type
-	 - body
-	 */
-
-	const socket = req.socket;
-	const reqUrl = url.parse(req.url, true);
-	const query = reqUrl.query;
-
-	if (query.stuck !== undefined) {
-		return;
-	}
-
-	const serve = function() {
-		if (query.reset !== undefined) {
-			socket.destroy();
-			return;
-		}
-
-		if (query.silent !== undefined) {
-			socket.end();
-			return;
-		}
-
-		const {status, headers, body} = getInitData(query);
-
-		res.writeHead(status, headers);
-		res.end(body);
-	};
-
-	if (isFinite(query.wait)) {
-		const wait = parseInt(query.wait);
-		setTimeout(serve, wait);
-		return;
-	}
-
-	serve();
-}
+const {host, port, cert, key} = args.parse();
 
 let server;
 if (cert && key) {
@@ -105,5 +18,5 @@ if (cert && key) {
 	server = http.createServer()
 }
 server.timeout = 0;
-server.on('request', onRequest);
+server.on('request', requestHandler);
 server.listen(port, host);
